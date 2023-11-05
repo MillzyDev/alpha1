23d9c8cbb0a457c691bf2fe52f6fb9a2256481f9
Millzy <millzyyt08@gmail.com>
2023-11-05 21:23:52 +0000

    move to namespace

--- shared/modloader.hpp
+++ shared/modloader.hpp
@@ -4,25 +4,27 @@
 
 #define ALPHA1_API extern "C" __declspec(dllexport)
 
-struct Alpha1Library {
-    HMODULE handle;
-    wchar_t *name;
-    HRESULT error;
-};
+namespace alpha1 {
+    struct Library {
+        HMODULE handle;
+        wchar_t *name;
+        HRESULT error;
+    };
 
-struct ModInfo {
-    wchar_t *name;
-    wchar_t *version;
-};
+    struct ModInfo {
+        wchar_t *name;
+        wchar_t *version;
+    };
 
-typedef void (*setup_func)(ModInfo &info);
-typedef void (*load_func)();
+    typedef void (*setup_func)(ModInfo &info);
+    typedef void (*load_func)();
 
-struct Alpha1Mod {
-    HMODULE handle;
-    wchar_t *name;
-    HRESULT error;
-    ModInfo info;
-    setup_func setup;
-    load_func load;
-};
+    struct Mod {
+        HMODULE handle;
+        wchar_t *name;
+        HRESULT error;
+        ModInfo info;
+        setup_func setup;
+        load_func load;
+    };
+}
