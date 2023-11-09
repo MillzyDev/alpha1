37b10afb6da53ad75016502d536aea76f55ac861
Millzy <millzyyt08@gmail.com>
2023-11-09 20:03:22 +0000

    fix spacing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
-#include "modloader.hpp"
+#include "modloader_internal.hpp"
+#include "files.hpp"
 
 #include <windows.h>
 
- ALPHA1_API [[maybe_unused]] void modloader_init() {
+void create_paths() {
+
+}
+
+ALPHA1_API [[maybe_unused]] void modloader_init() {
+    load_libs(get_libs_dir());
+
     MessageBoxA(nullptr, "Modloader loaded!", "Hi from alpha1!", MB_OK);
 }
