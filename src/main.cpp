56441a481c8e0ec929c0b7f6b985b10287990953
Millzy <millzyyt08@gmail.com>
2023-11-09 20:07:12 +0000

    create paths

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,16 @@
 #include <windows.h>
 
 void create_paths() {
-
+    if (!exists(get_libs_dir()))
+        std::filesystem::create_directory(get_libs_dir());
+    if (!exists(get_user_data_dir()))
+        std::filesystem::create_directory(get_user_data_dir());
+    if (!exists(get_mods_dir()))
+        std::filesystem::create_directory(get_mods_dir());
 }
 
 ALPHA1_API [[maybe_unused]] void modloader_init() {
+    create_paths();
     load_libs(get_libs_dir());
 
     MessageBoxA(nullptr, "Modloader loaded!", "Hi from alpha1!", MB_OK);
