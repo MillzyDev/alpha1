8da09cf865f0d9089028669919efa1d722532ee6
Millzy <millzyyt08@gmail.com>
2023-11-10 18:21:50 +0000

    fix type names

--- include/modloader_internal.hpp
+++ include/modloader_internal.hpp
@@ -5,5 +5,5 @@
 
 #include "modloader.hpp"
 
-std::vector<alpha1::Library> load_libs(const std::filesystem::path &libs_dir);
-std::vector<alpha1::Mod> loads_mods(const std::filesystem::path &mods_dir);
+std::vector<alpha1::library> load_libs(const std::filesystem::path &libs_dir);
+std::vector<alpha1::mod> loads_mods(const std::filesystem::path &mods_dir);
