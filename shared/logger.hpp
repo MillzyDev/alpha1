fa738005803f1979d88627cb9283090b6229e7f6
Millzy <millzyyt08@gmail.com>
2023-11-10 18:18:02 +0000

    untested logger

---
+++ shared/logger.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "modloader.hpp"
+
+#include <string>
+#include <format>
+
+namespace alpha1 {
+    enum log_level {
+        NONE,
+        INFO,
+        ERR,
+        CRITICAL,
+        WARNING,
+        DEBUG
+    };
+
+    void init_logger(bool enable_logging, bool enable_console);
+    void log(log_level level, const std::string& source_name, const std::string& message);
+
+    class logger  {
+    private:
+        const alpha1::mod_info mod_info_;
+
+    public:
+        explicit logger(alpha1::mod_info mod_info);
+
+        void log(log_level level, const std::string& message);
+
+        template<typename... Args>
+        void info(std::format_string<Args...> fmt, Args &&...args);
+
+        template<typename... Args>
+        void warn(std::format_string<Args...> fmt, Args &&...args);
+
+        template<typename... Args>
+        void error(std::format_string<Args...> fmt, Args &&...args);
+
+        template<typename... Args>
+        void crit(std::format_string<Args...> fmt, Args &&...args);
+
+        template<typename... Args>
+        void debug(std::format_string<Args...> fmt, Args &&...args);
+    };
+}
---
+++ src/logger.cpp
@@ -0,0 +1,137 @@
+#include "logger.hpp"
+#include "files.hpp"
+
+#include <fstream>
+#include <filesystem>
+#include <chrono>
+#include <iostream>
+
+std::ofstream log_file_stream;
+bool disable_logging;
+bool use_console;
+
+namespace alpha1 {
+    void init_logger(bool no_logging, bool enable_console) {
+        std::filesystem::path logs_dir = get_logs_dir();
+        std::filesystem::path latest_log = logs_dir / "latest.log";
+
+        if (exists(latest_log)) {
+            std::filesystem::file_time_type file_time = std::filesystem::last_write_time(latest_log);
+            auto system_time = std::chrono::clock_cast<std::chrono::system_clock>(file_time);
+            auto date_time = std::chrono::current_zone()->to_local(system_time);
+
+            std::string log_file_name = std::format("alpha1_{:%Y-%m-%d_%h-%M-%S}.log", date_time);
+            std::filesystem::path full_log_file_name = logs_dir / log_file_name;
+
+            std::filesystem::rename(latest_log, full_log_file_name);
+        }
+
+        disable_logging = no_logging;
+        if (no_logging)
+            return;
+
+        log_file_stream = std::ofstream(latest_log);
+
+        if (!enable_console) {
+            if (AllocConsole()) {
+                freopen("CONOUT$", "w", stdout);
+                use_console = true;
+            }
+            else {
+                log(log_level::ERR,
+                    "alpha1",
+
+                    "Unable to create console. It may be the case that one already exists, "
+                    "in which case alpha1 will not use it to prevent conflicts."
+                );
+            }
+        }
+    }
+
+    void log(log_level level, const std::string& source_name, const std::string& message) {
+        if (disable_logging || level == log_level::NONE) return;
+
+        auto now = std::chrono::system_clock::now();
+        auto seconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+        auto fraction = now - seconds;
+        auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(fraction);
+
+        std::string partial_timestamp = std::format("{:%h-%M-%S}", now);
+        std::string timestamp = std::format("{}.{}", partial_timestamp, static_cast<int>(milliseconds.count()));
+
+        std::string level_string;
+        int color_attribute;
+
+        switch (level) {
+            case INFO:
+                level_string = "INFO";
+                color_attribute = 10;
+                break;
+            case ERR:
+                level_string = "ERROR";
+                color_attribute = 12;
+                break;
+            case CRITICAL:
+                level_string = "CRITICAL";
+                color_attribute = 12;
+                break;
+            case DEBUG:
+                level_string = "DEBUG";
+                color_attribute = 9;
+                break;
+            case WARNING:
+                level_string = "WARN";
+                color_attribute = 14;
+                break;
+            case NONE:
+                break;
+        }
+
+        std::string full_message = std::format("[{}] [{}] [{}] {}", timestamp, source_name, level_string, message);
+        log_file_stream << full_message << std::endl;
+
+        if (use_console) {
+            static HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleTextAttribute(hConsole, color_attribute);
+            std::cout << full_message << std::endl;
+        }
+    }
+
+    logger::logger(alpha1::mod_info mod_info) {
+        this->mod_info_ = mod_info;
+    }
+
+    void logger::log(log_level level, const std::string& message) {
+        alpha1::log(level, this->mod_info_.name, message);
+    }
+
+    template<typename... Args>
+    void logger::debug(std::format_string<Args...> fmt, Args &&... args) {
+        std::string message = std::format(fmt, args...);
+        log(log_level::DEBUG, message);
+    }
+
+    template<typename... Args>
+    void logger::crit(std::format_string<Args...> fmt, Args &&... args) {
+        std::string message = std::format(fmt, args...);
+        log(log_level::CRITICAL, message);
+    }
+
+    template<typename... Args>
+    void logger::error(std::format_string<Args...> fmt, Args &&... args) {
+        std::string message = std::format(fmt, args...);
+        log(log_level::ERR, message);
+    }
+
+    template<typename... Args>
+    void logger::warn(std::format_string<Args...> fmt, Args &&... args) {
+        std::string message = std::format(fmt, args...);
+        log(log_level::WARNING, message);
+    }
+
+    template<typename... Args>
+    void logger::info(std::format_string<Args...> fmt, Args &&...args) {
+        std::string message = std::format(fmt, args...);
+        log(log_level::INFO, message);
+    }
+}
