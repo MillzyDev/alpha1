9b2eaf7cae87443d171e69e9349d607f559cf1c9
Millzy <millzyyt08@gmail.com>
2023-11-12 22:27:41 +0000

    fix typo

--- include/modloader_internal.hpp
+++ include/modloader_internal.hpp
@@ -6,4 +6,4 @@
 #include "modloader.hpp"
 
 std::vector<alpha1::library> load_libs(const std::filesystem::path &libs_dir);
-std::vector<alpha1::mod> loads_mods(const std::filesystem::path &mods_dir);
+std::vector<alpha1::mod> load_mods(const std::filesystem::path &mods_dir);
