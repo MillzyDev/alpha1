0a842c62ba5d399beecc9d33491cc78441eda8a0
Millzy <millzyyt08@gmail.com>
2023-11-12 22:27:50 +0000

    logs

--- src/modloader.cpp
+++ src/modloader.cpp
@@ -15,29 +15,29 @@ std::vector<alpha1::library> load_libs(const std::filesystem::path &libs_dir) {
         std::string filename = file_entry.path().filename().string();
         get_logger().info("Attempting to load {}", filename);
 
-        if (!GetModuleHandleW(module_path)) {
-            HMODULE lib_handle = LoadLibraryW(module_path);
+        HMODULE lib_handle = LoadLibraryW(module_path);
 
-            if (!lib_handle)
-                get_logger().error("Failed to load {}", filename);
-            else
-                get_logger().info("Successfully loaded {}", filename);
+        if (!lib_handle)
+            get_logger().error("Failed to load {}", filename);
+        else
+            get_logger().info("Successfully loaded {}", filename);
 
-            alpha1::library library(
-                lib_handle,
-                filename,
-                lib_handle ? NULL : GetLastError()
-            );
+        alpha1::library library(
+            lib_handle,
+            filename,
+            lib_handle ? NULL : GetLastError()
+        );
 
-            libs.push_back(library);
-        }
+        libs.push_back(library);
     }
 
-    get_logger().info("Finished loading ({}) libraries!", libs.size());
+    get_logger().info("Successfully loading ({}) libraries!", libs.size());
     return libs;
 }
 
 std::vector<alpha1::mod> load_mods(const std::filesystem::path &mods_dir) {
+    get_logger().info("Loading mods...");
+
     std::vector<alpha1::mod> mods;
 
     for (const auto& file_entry : std::filesystem::directory_iterator(mods_dir)) {
@@ -45,27 +45,35 @@ std::vector<alpha1::mod> load_mods(const std::filesystem::path &mods_dir) {
             continue;
 
         const wchar_t *module_path = file_entry.path().c_str();
-        if (!GetModuleHandleW(module_path)) {
-            HMODULE mod_handle = LoadLibraryW(module_path);
+        std::string filename = file_entry.path().filename().string();
+        get_logger().info("Attempting to load {}", filename);
 
-            auto setup_func = reinterpret_cast<alpha1::setup_func>(GetProcAddress(mod_handle, "setup"));
-            auto load_func = reinterpret_cast<alpha1::load_func>(GetProcAddress(mod_handle, "load"));
+        HMODULE mod_handle = LoadLibraryW(module_path);
 
-            alpha1::mod_info mod_info{};
-            setup_func(mod_info);
+        auto setup_func = reinterpret_cast<alpha1::setup_func>(GetProcAddress(mod_handle, "setup"));
+        auto load_func = reinterpret_cast<alpha1::load_func>(GetProcAddress(mod_handle, "load"));
 
-            alpha1::mod mod(
-                mod_handle,
-                file_entry.path().filename().string(),
-                mod_handle ? NULL : GetLastError(),
-                mod_info,
-                setup_func,
-                load_func
-            );
+        if (!mod_handle)
+            get_logger().error("Failed to load {}", filename);
+        else
+            get_logger().info("Successfully loaded {}", filename);
 
-            mods.push_back(mod);
-        }
+        alpha1::mod_info mod_info{};
+        setup_func(mod_info);
+        get_logger().info("Finished setup for {} v{}.", mod_info.name, mod_info.version);
+
+        alpha1::mod mod(
+            mod_handle,
+            file_entry.path().filename().string(),
+            mod_handle ? NULL : GetLastError(),
+            mod_info,
+            setup_func,
+            load_func
+        );
+
+        mods.push_back(mod);
     }
 
+    get_logger().info("Successfully loaded ({}) mods.", mods.size());
     return mods;
 }
