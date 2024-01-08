20d38e415d06fa02da481bf1c12e233cc3b66aa7
Millzy <millzyyt08@gmail.com>
2024-01-08 22:53:42 +0000

    hook with dobby

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -2,12 +2,21 @@
 #include "main.hpp"
 #include "modloader_internal.hpp"
 #include "files.hpp"
-
-#include "rcmp.hpp"
+#include "dobby.h"
 
 #include <windows.h>
 
-// TODO: Replace rcmp with new hooking lib - crash.
+int (*orig_il2cpp_init)(const char *domain_name);
+
+int hook_il2cpp_init(const char *domain_name) {
+    get_logger().info("Initialising in domain: {}", domain_name);
+    int ret = orig_il2cpp_init(domain_name); // run original function
+
+    // load mods
+    (void)load_mods(get_mods_dir());
+
+    return ret; // return original value
+}
 
 void install_hooks() {
     get_logger().info("Installing hooks...");
@@ -16,15 +25,8 @@ void install_hooks() {
     auto il2cpp_init_func = reinterpret_cast<void *>(GetProcAddress(game_assembly, "il2cpp_init"));
 
     get_logger().info("Installing il2cpp_init ({}) hook...", il2cpp_init_func);
-    rcmp::hook_function<int(*)(const char *)>(il2cpp_init_func, [](auto original, const char *domain_name) {
-        get_logger().info("IL2CPP initialised.");
-        get_logger().info("Domain name: {}", domain_name);
-        //get_logger().info("Orig returned: {}", ret);
 
-        load_mods(get_mods_dir());
-
-        return original(domain_name);
-    });
+    DobbyHook(il2cpp_init_func, reinterpret_cast<void *>(hook_il2cpp_init), (reinterpret_cast<void **>(&orig_il2cpp_init)));
 
     get_logger().info("Installed all hooks.");
 }
