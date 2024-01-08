bb65caa224118c92a0c52630002c6abcd53dea4b
Millzy <millzyyt08@gmail.com>
2024-01-08 23:21:22 +0000

    fixed skipping loading dlls due to bad extension check

--- src/modloader.cpp
+++ src/modloader.cpp
@@ -7,20 +7,20 @@ std::vector<alpha1::library> load_libs(const std::filesystem::path &libs_dir) {
 
     std::vector<alpha1::library> libs;
 
-    for (const auto& file_entry : std::filesystem::directory_iterator(libs_dir)) {
-        if (!file_entry.is_regular_file() || file_entry.path().extension() != "dll")
+    for (const auto& file_entry : std::filesystem::directory_iterator{libs_dir}) {
+        if (!file_entry.is_regular_file() || file_entry.path().extension().string() != ".dll") {
             continue;
+        }
 
         const wchar_t *module_path = file_entry.path().c_str();
         std::string filename = file_entry.path().filename().string();
-        get_logger().info("Attempting to load {}", filename);
 
         HMODULE lib_handle = LoadLibraryW(module_path);
 
         if (!lib_handle)
-            get_logger().error("Failed to load {}", filename);
+            get_logger().error("----- Library failed to load: {}", filename);
         else
-            get_logger().info("Successfully loaded {}", filename);
+            get_logger().info("----- Library loaded: {}", filename);
 
         alpha1::library library(
             lib_handle,
@@ -31,7 +31,7 @@ std::vector<alpha1::library> load_libs(const std::filesystem::path &libs_dir) {
         libs.push_back(library);
     }
 
-    get_logger().info("Successfully loading ({}) libraries!", libs.size());
+    get_logger().info("----- Successfully loading ({}) libraries!", libs.size());
     return libs;
 }
 
@@ -41,12 +41,11 @@ std::vector<alpha1::mod> load_mods(const std::filesystem::path &mods_dir) {
     std::vector<alpha1::mod> mods;
 
     for (const auto& file_entry : std::filesystem::directory_iterator(mods_dir)) {
-        if (!file_entry.is_regular_file() || file_entry.path().extension() != "dll")
+        if (!file_entry.is_regular_file() || file_entry.path().extension().string() != ".dll")
             continue;
 
         const wchar_t *module_path = file_entry.path().c_str();
         std::string filename = file_entry.path().filename().string();
-        get_logger().info("Attempting to load {}", filename);
 
         HMODULE mod_handle = LoadLibraryW(module_path);
 
@@ -54,13 +53,13 @@ std::vector<alpha1::mod> load_mods(const std::filesystem::path &mods_dir) {
         auto load_func = reinterpret_cast<alpha1::load_func>(GetProcAddress(mod_handle, "load"));
 
         if (!mod_handle)
-            get_logger().error("Failed to load {}", filename);
+            get_logger().error("----- Mod failed to load: {}", filename);
         else
-            get_logger().info("Successfully loaded {}", filename);
+            get_logger().info("----- Mod loaded: {}", filename);
 
         alpha1::mod_info mod_info{};
         setup_func(mod_info);
-        get_logger().info("Finished setup for {} v{}.", mod_info.name, mod_info.version);
+        get_logger().info("----- Finished setup for {} v{}.", mod_info.name, mod_info.version);
 
         alpha1::mod mod(
             mod_handle,
@@ -74,6 +73,6 @@ std::vector<alpha1::mod> load_mods(const std::filesystem::path &mods_dir) {
         mods.push_back(mod);
     }
 
-    get_logger().info("Successfully loaded ({}) mods.", mods.size());
+    get_logger().info("----- Successfully loaded ({}) mods.", mods.size());
     return mods;
 }
