edc8484f08a250336c6d2774b37ef0c6741a09a9
Millzy <millzyyt08@gmail.com>
2024-01-11 19:42:28 +0000

    fix attempted libs and mods being inconsistent

--- include/modloader_internal.hpp
+++ include/modloader_internal.hpp
@@ -6,9 +6,6 @@
 #include "modloader.hpp"
 
 namespace alpha1::modloader {
-    static std::vector<alpha1::library> attempted_libs = {};
-    static std::vector<alpha1::mod> attempted_mods = {};
-
-    std::vector<alpha1::library> load_libs(const std::filesystem::path &libs_dir);
-    std::vector<alpha1::mod> load_mods(const std::filesystem::path &mods_dir);
+    void load_libs(const std::filesystem::path &libs_dir);
+    void load_mods(const std::filesystem::path &mods_dir);
 }
