11f999d020ec5dfb66b6a1e4907c99bfb2af4395
Millzy <millzyyt08@gmail.com>
2024-04-13 14:04:59 +0100

    mod info

---
+++ shared/modloader.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+
+#include <windows.h>
+
+namespace alpha1 {
+    struct mod_info {
+        std::string name;
+        std::string version;
+        std::string author;
+    };
+}
