df464cce72ba7a395817e4e1ea6852bb4691ac69
Millzy <millzyyt08@gmail.com>
2024-04-14 23:06:19 +0100

    logger test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <windows.h>
 
 #include "files.hpp"
+#include "logger.hpp"
 
 extern "C" __declspec(dllexport) void alpha1_init() {
     alpha1::create_logs_directory();
+    alpha1::logger::initialise_logger();
 
-
+    alpha1::logger my_logger({"alpha1", "0.1.0", ""});
+    my_logger.info("Successfully initialised logger!");
 }
