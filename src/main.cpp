270d0d507869508f734e80083a2f5b105c5b3f2c
Millzy <millzyyt08@gmail.com>
2024-04-16 00:28:13 +0100

    load libs

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,17 @@
 
 #include "files.hpp"
 #include "logger.hpp"
+#include "loading.hpp"
+
+static alpha1::logger main_logger({"alpha1", "0.1.0", ""});
 
 extern "C" __declspec(dllexport) void alpha1_init() {
     alpha1::create_logs_directory();
     alpha1::logger::initialise_logger();
+    main_logger.info("Successfully initialised logger!");
 
-    alpha1::logger my_logger({"alpha1", "0.1.0", ""});
-    my_logger.info("Successfully initialised logger!");
+    alpha1::create_directories(main_logger);
+
+    alpha1::load_libraries(main_logger);
+
 }
