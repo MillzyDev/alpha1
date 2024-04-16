90cce8b51736652d9a26b60a61668723623c6b99
Millzy <millzyyt08@gmail.com>
2024-04-16 10:47:26 +0100

    reduce ambiguity

--- shared/modloader.hpp
+++ shared/modloader.hpp
@@ -28,12 +28,12 @@ namespace alpha1 {
     struct mod_metadata {
         std::filesystem::path filepath;
         HMODULE handle;
-        mod_info info;
-        load_t load_func;
-        start_t start_func;
-        scene_loaded_t scene_loaded_func;
-        scene_unloaded_t scene_unloaded_func;
-        quit_t quit_func;
+        ::alpha1::mod_info info;
+        ::alpha1::load_t load_func;
+        ::alpha1::start_t start_func;
+        ::alpha1::scene_loaded_t scene_loaded_func;
+        ::alpha1::scene_unloaded_t scene_unloaded_func;
+        ::alpha1::quit_t quit_func;
     };
 
     struct library_metadata {
@@ -48,22 +48,22 @@ namespace alpha1 {
     };
 
     class ALPHA1_EXPORT modloader {
-        friend void add_mod(mod_metadata metadata);
-        friend void add_library(library_metadata metadata);
-        friend void add_failed_mod(failed_library mod);
-        friend void add_failed_library(failed_library library);
+        friend void add_mod(::alpha1::mod_metadata metadata);
+        friend void add_library(::alpha1::library_metadata metadata);
+        friend void add_failed_mod(::alpha1::failed_library mod);
+        friend void add_failed_library(::alpha1::failed_library library);
 
     private:
-        static std::vector<mod_metadata> loaded_mods;
-        static std::vector<library_metadata> loaded_libraries;
-        static std::vector<failed_library> failed_mods;
-        static std::vector<failed_library> failed_libraries;
+        static std::vector<::alpha1::mod_metadata> loaded_mods;
+        static std::vector<::alpha1::library_metadata> loaded_libraries;
+        static std::vector<::alpha1::failed_library> failed_mods;
+        static std::vector<::alpha1::failed_library> failed_libraries;
 
     public:
-        [[nodiscard]] bool get_mod_info(std::string mod_name, mod_metadata *metadata);
-        [[nodiscard]] std::vector<mod_metadata> get_mods();
-        [[nodiscard]] std::vector<library_metadata> get_libraries();
-        [[nodiscard]] std::vector<failed_library> get_failed_mods();
-        [[nodiscard]] std::vector<failed_library> get_failed_libraries();
+        [[nodiscard]] bool get_mod_info(std::string mod_name, ::alpha1::mod_metadata *metadata);
+        [[nodiscard]] std::vector<::alpha1::mod_metadata> get_mods();
+        [[nodiscard]] std::vector<::alpha1::library_metadata> get_libraries();
+        [[nodiscard]] std::vector<::alpha1::failed_library> get_failed_mods();
+        [[nodiscard]] std::vector<::alpha1::failed_library> get_failed_libraries();
     };
 }
--- src/modloader.cpp
+++ src/modloader.cpp
@@ -4,37 +4,37 @@
 #include <algorithm>
 
 namespace alpha1 {
-    void add_mod(alpha1::mod_metadata metadata) {
+    void add_mod(::alpha1::mod_metadata metadata) {
         modloader::loaded_mods.push_back(metadata);
     }
 
-    void add_library(alpha1::library_metadata metadata) {
+    void add_library(::alpha1::library_metadata metadata) {
         modloader::loaded_libraries.push_back(metadata);
     }
 
-    void add_failed_mod(alpha1::failed_library mod) {
+    void add_failed_mod(::alpha1::failed_library mod) {
         modloader::failed_mods.push_back(mod);
     }
 
-    void add_failed_library(alpha1::failed_library library) {
+    void add_failed_library(::alpha1::failed_library library) {
         modloader::failed_libraries.push_back(library);
     }
 
-    std::vector<mod_metadata> modloader::loaded_mods;
-    std::vector<library_metadata> modloader::loaded_libraries;
-    std::vector<failed_library> modloader::failed_mods;
-    std::vector<failed_library> modloader::failed_libraries;
+    std::vector<::alpha1::mod_metadata> modloader::loaded_mods;
+    std::vector<::alpha1::library_metadata> modloader::loaded_libraries;
+    std::vector<::alpha1::failed_library> modloader::failed_mods;
+    std::vector<::alpha1::failed_library> modloader::failed_libraries;
 
-    bool modloader::get_mod_info(std::string mod_name, mod_metadata *metadata) {
-        static std::map<std::string, mod_metadata> cache;
+    bool modloader::get_mod_info(std::string mod_name, ::alpha1::mod_metadata *metadata) {
+        static std::map<std::string, ::alpha1::mod_metadata> cache;
 
         if (cache.contains(mod_name)) {
             *metadata = cache[mod_name];
             return true;
         }
 
-        auto it = std::find_if(modloader::loaded_mods.begin(), modloader::loaded_mods.end(),
-                               [mod_name](mod_metadata metadata) -> bool {
+        auto it = std::find_if(::alpha1::modloader::loaded_mods.begin(), ::alpha1::modloader::loaded_mods.end(),
+                               [mod_name](::alpha1::mod_metadata metadata) -> bool {
             return metadata.info.name == mod_name;
         });
 
@@ -47,20 +47,20 @@ namespace alpha1 {
         return false;
     }
 
-    std::vector<mod_metadata> modloader::get_mods() {
-        return modloader::loaded_mods;
+    std::vector<::alpha1::mod_metadata> modloader::get_mods() {
+        return ::alpha1::modloader::loaded_mods;
     }
 
-    std::vector<library_metadata> modloader::get_libraries() {
-        return modloader::loaded_libraries;
+    std::vector<::alpha1::library_metadata> modloader::get_libraries() {
+        return ::alpha1::modloader::loaded_libraries;
     }
 
-    std::vector<failed_library> modloader::get_failed_mods() {
-        return modloader::failed_mods;
+    std::vector<::alpha1::failed_library> modloader::get_failed_mods() {
+        return ::alpha1::modloader::failed_mods;
     }
 
-    std::vector<failed_library> modloader::get_failed_libraries() {
-        return modloader::failed_libraries;
+    std::vector<::alpha1::failed_library> modloader::get_failed_libraries() {
+        return ::alpha1::modloader::failed_libraries;
     }
 
 }
