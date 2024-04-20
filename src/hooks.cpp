d944b31dd8cb34b7c2f4b28be65b2b7862f1a26e
Millzy <millzyyt08@gmail.com>
2024-04-20 16:21:05 +0100

    init hook do stuff

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -2,20 +2,27 @@
 #include "hooking.hpp"
 #include "il2cpp.hpp"
 #include "logger.hpp"
+#include "loading.hpp"
 
 static ::alpha1::logger *hook_logger = nullptr;
-static il2cpp_init_t orig = nullptr;
+static il2cpp_init_t il2cpp_init_orig = nullptr;
 
 int il2cpp_init_detour(const char *domain_name) {
-    hook_logger->info("Domain name: {0}", domain_name);
-    return orig(domain_name);
+    int ret = il2cpp_init_orig(domain_name);
+
+    alpha1::load_mods(*hook_logger);
+
+    ::alpha1::hook_runtime();
+
+    return ret;
 }
 
 namespace alpha1 {
     void hook_init(::alpha1::logger &logger) {
         hook_logger = &logger;
-        orig = ::alpha1::il2cpp::init;
-        hook(reinterpret_cast<void **>(&orig), reinterpret_cast<void *>(il2cpp_init_detour));
+
+        il2cpp_init_orig = ::alpha1::il2cpp::init;
+        hook(reinterpret_cast<void **>(&il2cpp_init_orig), reinterpret_cast<void *>(il2cpp_init_detour));
     }
 
     void hook_runtime() {
